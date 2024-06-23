9e73e3b3fd6dac1dd3b6999837753ea332ae65be
ghina habib <ghinahabib55@gmail.com>
2024-06-23 22:48:57 +0300

    ++

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: camel_case_types, must_be_immutable, library_private_types_in_public_api, use_key_in_widget_constructors
 
-import 'package:chic_events/const.dart';
+import 'package:chic_events/helper/package.dart';
 import 'package:chic_events/screens/one.dart';
 import 'package:chic_events/widgets/button.dart';
 
@@ -291,68 +291,6 @@ class help extends StatelessWidget {
           style: const TextStyle(fontSize: 14),
         )
       ],
-    );
-  }
-}
-
-class package extends StatelessWidget {
-  package(
-      {required this.name,
-      @required this.guest,
-      required this.photo,
-      required this.price});
-  String? name;
-  String? guest;
-  String? price;
-  String? photo;
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(10),
-          boxShadow: [
-            BoxShadow(
-                color: Colors.grey.withOpacity(0.3),
-                spreadRadius: 3,
-                blurRadius: 9,
-                offset: const Offset(0, 3))
-          ]),
-      height: 200,
-      width: 180,
-      clipBehavior: Clip.hardEdge,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Image.asset(
-            photo!,
-            fit: BoxFit.cover,
-          ),
-          const SizedBox(
-            height: 8,
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 5),
-            child: Text(name!),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 5),
-            child: Text(
-              guest!,
-              style: const TextStyle(color: color4, fontSize: 14),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 5),
-            child: Text(
-              ' \$ $price onwards',
-              style: const TextStyle(
-                color: color2,
-              ),
-            ),
-          )
-        ],
-      ),
     );
   }
 }
